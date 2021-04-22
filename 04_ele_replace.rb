a84bf1e422b3ab517645ea61a2c2f95a693c0337
regine-tiu <regine.d.tiu@gmail.com>
2021-04-22 15:59:47 +0800

    solved

--- 04_ele_replace.rb
+++ 04_ele_replace.rb
@@ -7,7 +7,13 @@
 # to be the same before and after calling your method.
 
 def ele_replace!(array, hash)
-
+  array.map! do |ele|
+    if hash.has_key?(ele)
+      hash[ele]
+    else
+      ele  
+    end  
+  end 
 end
 
 
